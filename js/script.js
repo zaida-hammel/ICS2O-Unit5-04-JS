635f644662443bfca97fe52d5e3d824b1d53fcdc
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-02 17:29:40 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Zaida Hammel
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML/",
+  });
+}
+
+/**
+ * This function determines if an integer is negative or positive.
+ */
 function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+  var dayWeek = document.getElementById("days").value
+  var userAge = parseInt(document.getElementById("userAge").value)
+  const tuesday = document.getElementById("tuesday").value
+  const thursday = document.getElementById("thursday").value
+  if ((dayWeek == tuesday) || (dayWeek == thursday) || (userAge > 12 && userAge < 21)) {
+    document.getElementById("answers").innerHTML = "You have a student discount."
+  } else {
+    document.getElementById("answers").innerHTML = "You do not have a discount, pay regular pricing."
+  }
 }
