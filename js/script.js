2d87751053c6856ddfd5a5e55f51c00b30ae291c
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-03 17:20:33 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -8,21 +8,23 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-01-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit5-04-JS/",
   });
 }
 
 /**
- * This function determines if an integer is negative or positive.
+ * This function determines if you are eligible for student discount.
  */
 function myButtonClicked() {
-  var dayWeek = document.getElementById("days").value
   var userAge = parseInt(document.getElementById("userAge").value)
-  const tuesday = document.getElementById("tuesday").value
-  const thursday = document.getElementById("thursday").value
-  if ((dayWeek == tuesday) || (dayWeek == thursday) || (userAge > 12 && userAge < 21)) {
-    document.getElementById("answers").innerHTML = "You have a student discount."
+
+  var tuesday = document.getElementById("tuesday")
+  var thursday = document.getElementById("thursday")
+
+  
+  if ((tuesday == true) || (thursday == true) || (userAge > 12 && userAge < 21)) {
+    document.getElementById("answers").innerHTML = "You have a student discount! Pay student pricing."
   } else {
     document.getElementById("answers").innerHTML = "You do not have a discount, pay regular pricing."
   }
